2bdb2d5f48867ca222782e3841eeb94a19e60a16
Adam Spindler <xmeadamx@gmail.com>
2021-02-09 17:34:03 -0500

    Remove submodule

---
+++ yolo-v3-adversarial/main.py
@@ -0,0 +1,13 @@
+"""
+output:
+[1, 10647, 85]
+
+10647 possible bounding boxes with labels
+    0: bbox center x
+    1: bbox center y
+    2: bbox width
+    3: bbox height
+    4: bbox existence confidence
+    5-85: classification scores
+"""
+
