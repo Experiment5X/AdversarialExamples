6a665a567f482cf239779683149a3803d14d7b41
Adam Spindler <xmeadamx@gmail.com>
2021-02-25 00:00:42 -0500

    Make ensemble adversarial images robust to JPG

--- detection_experiments/ensemble_adversarial.py
+++ detection_experiments/ensemble_adversarial.py
@@ -1,5 +1,6 @@
 import sys
 import torch
+import math
 import random
 import numpy as np
 from PIL import Image
@@ -13,6 +14,49 @@ from predict_yolo import predict_image_tensor as predict_yolo
 image_path = sys.argv[1]
 to_tensor = ToTensor()
 to_pil_image = ToPILImage()
+
+
+def gaussian_blur(image):
+    # Set these to whatever you want for your gaussian filter
+    kernel_size = 3
+    sigma = 0.25
+    channels = 3
+
+    # Create a x, y coordinate grid of shape (kernel_size, kernel_size, 2)
+    x_cord = torch.arange(kernel_size)
+    x_grid = x_cord.repeat(kernel_size).view(kernel_size, kernel_size)
+    y_grid = x_grid.t()
+    xy_grid = torch.stack([x_grid, y_grid], dim=-1)
+
+    mean = (kernel_size - 1) / 2.0
+    variance = sigma ** 2.0
+
+    # Calculate the 2-dimensional gaussian kernel which is
+    # the product of two gaussian distributions for two different
+    # variables (in this case called x and y)
+    gaussian_kernel = (1.0 / (2.0 * math.pi * variance)) * torch.exp(
+        -torch.sum((xy_grid - mean) ** 2.0, dim=-1) / (2 * variance)
+    )
+    # Make sure sum of values in gaussian kernel equals 1.
+    gaussian_kernel = gaussian_kernel / torch.sum(gaussian_kernel)
+
+    # Reshape to 2d depthwise convolutional weight
+    gaussian_kernel = gaussian_kernel.view(1, 1, kernel_size, kernel_size)
+    gaussian_kernel = gaussian_kernel.repeat(3, 1, 1, 1)
+
+    gaussian_filter = torch.nn.Conv2d(
+        in_channels=channels,
+        out_channels=channels,
+        kernel_size=kernel_size,
+        groups=channels,
+        bias=False,
+        padding=1,
+    )
+
+    gaussian_filter.weight.data = gaussian_kernel
+    gaussian_filter.weight.requires_grad = False
+
+    return gaussian_filter(image)
 
 
 def create_adversarial(image_path):
@@ -30,18 +74,18 @@ def create_adversarial(image_path):
 
         rccn_detections = rcnn_model.forward(image_tensor)
         all_confidence_scores_rcnn = process_prediction(rccn_detections)
+        rcnn_loss = torch.norm(all_confidence_scores_rcnn, 2)
 
         yolo_detections = yolo_model.forward(image_tensor)
-        yolo_adversarial_loss = get_adversarial_loss(yolo_detections)
+        yolo_loss = get_adversarial_loss(yolo_detections) / 200
 
         yolo_detection_names = get_prediction_names(yolo_detections, yolo_classes)
         print(f'YOLOv3 Detections: {yolo_detection_names}')
 
-        adversarial_loss = (
-            10 * torch.norm(all_confidence_scores_rcnn, 2)
-            + 0.5 * yolo_adversarial_loss
-            + 0.1 * torch.norm(orginal_image_tensor - image_tensor, 2)
+        image_diff_regularization = (
+            torch.norm(orginal_image_tensor - image_tensor, 2) / 25
         )
+        adversarial_loss = rcnn_loss + yolo_loss + image_diff_regularization
         adversarial_loss.backward()
 
         image_tensor = image_tensor.data - 0.01 * torch.sign(image_tensor.grad.data)
@@ -49,7 +93,13 @@ def create_adversarial(image_path):
             image_tensor.max() - image_tensor.min()
         )
 
-        print(f'[{iteration}] Adversarial loss: {adversarial_loss:.5f}')
+        if iteration % 3 == 0 and iteration != 0 and iteration < 29:
+            image_tensor = gaussian_blur(image_tensor)
+            print('\tAdding gassian blur')
+
+        print(
+            f'[{iteration}] Adversarial loss total: {adversarial_loss:.5f}, RCNN Loss: {rcnn_loss:.5f}, YOLO Loss: {yolo_loss:.5f}, Image Diff: {image_diff_regularization:.5f}'
+        )
         print()
 
     return image_tensor
@@ -59,8 +109,12 @@ adversarial_image_tensor = create_adversarial(image_path)
 adversarial_image = to_pil_image(adversarial_image_tensor.squeeze(0))
 
 image_id = random.randint(0, 1000)
-adversarial_image.save(f'./ensemble_adversarial-{image_id}.png')
+adversarial_image_path = f'./ensemble_adversarial-{image_id}.png'
+adversarial_image.save(adversarial_image_path)
+
+reloaded_image = Image.open(adversarial_image_path)
+reloaded_image_tensor = to_tensor(reloaded_image).unsqueeze(0)
 
 print('Final adversarial image predictions...')
-predict_rcnn(adversarial_image_tensor)
-predict_yolo(adversarial_image_tensor)
+predict_rcnn(reloaded_image_tensor)
+predict_yolo(reloaded_image_tensor)
